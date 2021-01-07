bc0a30bd1f500907964398ec9de62363d05b472b
Cooper Brown <cp_brown@outlook.com>
2021-01-07 15:15:43 -0700

    style changes

--- src/distancematrix.f90
+++ src/distancematrix.f90
@@ -1,6 +1,6 @@
 module distancematrix_mod
 use global
-use blas95
+use blas95, only: gemm
 implicit none
 private
 public distancematrix
@@ -9,29 +9,55 @@ contains
 
 subroutine distancematrix(datasites, centers, dmat, error)
 
-    real(dp), intent(in) :: datasites(:,:), centers(:,:)
+  ! --- INPUT DECLARATIONS --- !
+
+    ! Location of data: M x d matrix
+    real(dp), intent(in) :: datasites(:,:)
+    ! Evaluation points: d x N matrix
+    real(dp), intent(in) :: centers(:,:)
+
+  ! --- OUTPUT DECLARATIONS --- !
+
+    ! The distance matrix: M x N
     real(dp), allocatable, intent(out) :: dmat(:,:)
+    ! Error code
     integer, intent(out) :: error
 
-    integer :: N, M, d, k
-    real(dp), allocatable :: ss_dsites(:), ss_cntrs(:)
+  ! --- LOCAL DECLARATIONS --- !
 
+    ! Number of points in datasites and centers, respectively
+    integer :: N, M
+    ! Dimensionality of data
+    integer :: d
+    ! Temporary arrays to hold sums of squares
+    real(dp), allocatable :: ss_dsites(:), ss_cntrs(:)
+    ! Iterator
+    integer :: k
+    
+  ! --- SETUP --- !
+
+    ! Get sizes
     M = size(datasites, 1)
     N = size(centers, 2)
     d = size(datasites, 2)
 
-    ! Make sure size(centers, 1) is also d
+    ! Quit with error if dimensions aren't compatible
     if (size(centers, 1) /= d) then
         write(*,*) 'dimensions do not match'
         error = 1
         return
     end if
 
+    ! Allocate dmat
     if (allocated(dmat)) deallocate(dmat)
     allocate(dmat(M,N))
     
-    ! Algorithm
+  ! --- ALGORITHM --- !
+
+    ! Initializes dmat as -2*datasites*centers
     call gemm(datasites, centers, dmat, 'n', 'n', -2.0_dp)
+
+    ! Add matrix of sums of squares
     allocate(ss_dsites(M), ss_cntrs(N))
     ss_dsites = sum(datasites**2.0_dp, 2)
     ss_cntrs = sum(centers**2.0_dp, 1)
@@ -39,8 +65,11 @@ subroutine distancematrix(datasites, centers, dmat, error)
         dmat(:,k) = dmat(:,k) + ss_dsites(:) + ss_cntrs(k)
     end do
     deallocate(ss_dsites, ss_cntrs)
+
+    ! Take square root of all elements to finish
     dmat = dmat**0.5_dp
 
+    ! Successful exit
     error = 0
     return
 
